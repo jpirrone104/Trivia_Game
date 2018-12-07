12331da79307c039596570fec892c51368fc27b9
Jen Pirrone <pirronej104@gmail.com>
2018-12-06 23:09:28 -0500

    thursday commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,7 @@
 //game variables
 var countDown = 25;
 
-function gamestart() {
+
     
     //available questions array
     var questionsAvail = [
@@ -10,7 +10,7 @@ function gamestart() {
         {
             question: "What is the name of Jim and Pam's first child?",
             options: ["Philip", "CeCe", "Astrid", "Dwight"],
-            answer: 1,
+            answer: "CeCe",
             correctpicture: "http://66.media.tumblr.com/6ab6a9cfc7535708d90f1409692ea13b/tumblr_oagggmfYqx1vqsw2co1_500.gif",
             
 
@@ -21,7 +21,7 @@ function gamestart() {
         {
             question: "What book does Jim ask Dwight to read him and Pam when they stay at his Bed and Breakfast?",
             options: ["Goodnight, Moon", "1984", "Harry Potter", "The Lord of the Rings"],
-            answer: 2,
+            answer: "Harry Potter",
             correctpicture: "https://i1.wp.com/gifrific.com/wp-content/uploads/2013/05/Michael-Scott-and-Dwight-Schrute-Raise-the-Roof.gif?zoom=1.25&ssl=1",
 
         }
@@ -29,37 +29,51 @@ function gamestart() {
 
     ]
 
-    //randomize questions
-    pickRandom = Math.floor(Math.random()*questionsAvail.length);
-    var questionSelected = questionsAvail[pickRandom];
+    function gamestart() {
+        //clear question div
+        $("#question").html("");
 
-    console.log(pickRandom);
+        //randomize questions
+        pickRandom = Math.floor(Math.random()*questionsAvail.length);
+        var questionSelected = questionsAvail[pickRandom];
+        console.log(pickRandom);
 
-    $("#question").html("<h2>" + questionsAvail[pickRandom].question + "</h2>");
-
-    console.log(questionsAvail[pickRandom].question);
-
-    $("#options").html(
-    "<ul>" +
-    "<li><button>" + questionsAvail[pickRandom].options[0] + "</button></li>" +
-    "<li><button>" + questionsAvail[pickRandom].options[1] + "</button></li>" +
-    "<li><button>" + questionsAvail[pickRandom].options[2] + "</button></li>" +
-    "<li><button>" + questionsAvail[pickRandom].options[3] + "</button></li>" +
-    "</ul>");
+        //update question on question div
+        $("#question").html("<h2>" + questionsAvail[pickRandom].question + "</h2>");
+        console.log(questionsAvail[pickRandom].question);
     
-    console.log(questionsAvail[pickRandom].options);
-    $("#timer").html("Time remaining: " + countDown + " seconds");
+        //render options on options div
+        function updateOptions() {
 
-    $(document).on("click", "li", function() {
-
-        if(countDown > 0 && $(this) === questionsAvail[pickRandom].answer) {
-            $("#question").html(questionsAvail[pickRandom].correctpicture);
-            $("#options").html("");
-            $("#timer").html(""); 
+            var optionButton = $("<button>")
+            
+            $.each(questionsAvail[pickRandom].options, function(index) {
+                var optionsIndex = questionsAvail[pickRandom].options[index];
+                console.log(optionsIndex);
+                $("#options").append("<button>" + optionsIndex + "</button>");
+                $("button").attr("id", optionsIndex).addClass("options");
+            });
         }
+
+            updateOptions();
         
-    });
-    
+
+            
+        $("#timer").html("Time remaining: " + countDown + " seconds");
+
+        $(document).on("click", ".option", function() {
+            alert($(this).answer)
+
+            if(countDown > 0 && $(this) === questionsAvail[pickRandom].answer) {
+
+                $("#options").html("");
+                $("#timer").html(""); 
+                $("#question").html(questionsAvail[pickRandom].correctpicture);
+                
+            }
+            
+        });
+        
 };
 
 function restart() {
